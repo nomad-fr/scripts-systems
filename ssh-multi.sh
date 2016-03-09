40056f2995b24ff3ad20c33c1387a0a5010a4555
Michel Le Cocq <nomad@neuronfarm.net>
2016-03-09 16:04:17 +0100

    some cleanup

--- ssh-multi.sh
+++ ssh-multi.sh
@@ -6,26 +6,25 @@
 
 # a script to ssh multiple servers over multiple tmux panes
 
-testssh() {
-    echo quit | telnet $1 22 2>/dev/null | grep Connected
-}
-
 starttmux() {
 
     if [ -z "$HOSTS" ]; then
            echo -n "Please provide of list of hosts separated by spaces [ENTER]: "
            read HOSTS
     fi
-
+ 
     local hosts=( $HOSTS )
     local target="ssh-multi ${host[0]}"
-
+    
     tmux new-window -n "${target}" ssh $user@${hosts[0]}
     unset hosts[0];
+    
     for i in "${hosts[@]}"
     do
-        tmux split-window -t :"${target}" -h  "ssh $user@$i"
+
+        tmux split-window -t :"${target}" -h "ssh $user@$i"
         tmux select-layout -t :"${target}" tiled > /dev/null
+	
     done
     tmux select-pane -t 0
     tmux set-window-option -t :"${target}"  synchronize-panes on > /dev/null
