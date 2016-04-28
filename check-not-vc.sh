335bd924975abb9b29a034f8836ab6cc6e40a206
nomad-fr <nomad@neuronarm.net>
2016-04-28 10:55:17 +0200

    1er commit de check-not-vc.sh

---
+++ check-not-vc.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# check-not-vc.sh : a script to check a that everything in a Version Control
+#                   folder is under Version Control
+# nomad-fr : 28.04.2016
+
+usage() {
+    echo $0' "repository folder path"'
+    exit 0
+}
+
+check-svn() {
+    echo 'Untracked files:'
+    svn status $(svn info $path | head -2 | tail -1 | awk -F':' '{print $2}') | grep ^\?
+}
+
+check-git() {
+    git status 
+}
+
+check-vc() {
+    svn info $path &>/dev/null
+    if [ "$?" = 0 ]
+    then
+	check-svn
+    else
+	cd $path
+	git log &>/dev/null
+	check-git
+    fi		   
+}
+
+if [ "$#" -ne 1 ]; then usage; fi
+path=$1
+
+check-vc
+
