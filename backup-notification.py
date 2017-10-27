2bd5c29b3006790d8a2261841c3ee3f361e8488e
Michel Le Cocq <nomad@neuronfarm.net>
2017-10-27 15:01:21 +0200

    nomage element menu / changement icon / si time ou

--- backup-notification.py
+++ backup-notification.py
@@ -23,7 +23,7 @@ class IndicatorBackup:
             # finally, the category. We're monitoring, so SYSTEM_SERVICES
             self.indic = appindicator.Indicator.new(
                            'Backup Notification indicator app',
-                           os.path.abspath('/usr/share/icons/gnome/24x24/emotes/face-smile-big.png'),
+                           os.path.abspath('./icon-backup-notification/icon-pitit-chien-violet-p.png'),
                            appindicator.IndicatorCategory.SYSTEM_SERVICES)
 
             # some more information about the AppIndicator:
@@ -37,36 +37,63 @@ class IndicatorBackup:
             self.menu = gtk.Menu()
 
             # menu status
-            item = gtk.MenuItem()
-            item.set_label('Status')
-            #item.connect("activate", self.handler_menu_test)
-            item.set_sensitive(False)
-            item.show()
-            self.menu.append(item)
+            status = gtk.MenuItem()
+            status.set_label('Status : nothing yet')
+            status.set_sensitive(False)
+            status.show()
+            self.menu.append(status)
 
             # menu Backup
-            item = gtk.MenuItem()
-            item.set_label("Backup")
-            item.connect("activate", self.backup)
-            item.show()
-            self.menu.append(item)
+            backup = gtk.MenuItem()
+            backup.set_label("Backup")
+            backup.connect("activate", self.backup)
+            backup.show()
+            self.menu.append(backup)
             
             # menu quit
-            item = gtk.MenuItem()
-            item.set_label("Quit")
-            item.connect("activate", self.handler_menu_exit)
-            item.show()
-            self.menu.append(item)
+            out = gtk.MenuItem()
+            out.set_label("Quit")
+            out.connect("activate", self.handler_menu_exit)
+            out.show()
+            self.menu.append(out)
 
             self.menu.show()
             self.indic.set_menu(self.menu)
 
-            # # initialize cpu speed display
-            # self.update_cpu_speeds()
-            # # then start updating every 2 seconds
-            # # http://developer.gnome.org/pygobject/stable/glib-functions.html#function-glib--timeout-add-seconds
-            # GLib.timeout_add_seconds(2, self.handler_timeout)
-            
+            # initialize initial status
+            #self.check_status()
+            # then start updating every 2 seconds
+            #GLib.timeout_add_seconds(2, self.check_status)
+
+    # def get_cpu_speeds(self):
+    #     """Use regular expression to parse speeds of all CPU cores from
+    #     /proc/cpuinfo on Linux.
+    #     """
+
+    #     f = open('/proc/cpuinfo')
+    #     # this gives us e.g. ['2300', '2300']
+    #     s = re.findall('cpu MHz\s*:\s*(\d+)\.', f.read())
+    #     # this will give us ['2.3', '2.3']
+    #     f = ['%.1f' % (float(i) / 1000,) for i in s]
+    #     return f
+
+    def check_status(self):
+        self.indic.set_icon('/local/VersionControl/GitHub/nomad-fr/scripts-systems/icon-backup-notification/icon-pitit-chien-violet-pb.png')
+        stat=call(['bash', '/home/nomad/bin/backup-laptop-neuronfarm.sh', 'last'])
+        # return True so that we get called again
+        # returning False will make the timeout stop
+        
+        return True
+        
+    # def handler_timeout(self):
+    #     """This will be called every few seconds by the GLib.timeout.
+    #     """
+    #     # read, parse and put cpu speeds in the label
+    #     self.update_cpu_speeds()
+    #     # return True so that we get called again
+    #     # returning False will make the timeout stop
+    #     return True
+    
     def handler_menu_exit(self, evt):
         gtk.main_quit()
             
